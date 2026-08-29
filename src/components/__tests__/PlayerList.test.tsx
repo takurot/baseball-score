4454b905372ffa368b560456df894c6627228dd0
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2026-08-29 19:27:02 +0900

    fix: remove unused fireEvent import in PlayerList.test.tsx

--- src/components/__tests__/PlayerList.test.tsx
+++ src/components/__tests__/PlayerList.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import PlayerList, { registerAtBatButtonId } from '../PlayerList';
 import { Player } from '../../types';
